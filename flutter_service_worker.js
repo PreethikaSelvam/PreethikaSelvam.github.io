c49336d07761734c86c645efed7bbd7c6cfd34c9
PreethikaSelvam <preethika.selvam@syncfusion.com>
2024-01-05 11:14:08 +0530

    CustomerSampleNew

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9a510e47a52245633b9ed8890267eaee",
-"/": "9a510e47a52245633b9ed8890267eaee",
-"main.dart.js": "f5964084772bf1c00e3b26037031b1f5",
+"index.html": "ee3bed74b612a788fc79480da6684f76",
+"/": "ee3bed74b612a788fc79480da6684f76",
+"main.dart.js": "79f6e88f74a8d70cac3ad528372f8ef1",
 "manifest.json": "3c1105536eb51ac4632e348add4530b7",
 "version.json": "00f8c5adec4354ef5f89e89c32151470"};
 // The application shell files that are downloaded before a service worker can
